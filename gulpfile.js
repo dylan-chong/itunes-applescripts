dabe89b63583b5076b7c55273f7b6c3bdc8de002
Dylan Chong <dylan.chong@icloud.com>
2016-06-18 11:19:13 +1200

    Successfully added a way to get a file to string (gulpfile.js)

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const fs = require('fs');
+
+const SCRIPT_FILE = 'src/albumize-disc-groups.js.applescript';
+
+function log(tag, data) {
+    console.log('\n\n' + tag + '\n\n', data);
+}
 
 gulp.task('default', function () {
-    // default task
+    var scriptContents = fs.readFileSync(SCRIPT_FILE, 'utf8');
+    log('Script Contents', scriptContents);
+
 });
