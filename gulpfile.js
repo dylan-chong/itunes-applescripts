8bb903816c217e8f565e4cea09f15b4d21a74754
Dylan Chong <dylan.chong@icloud.com>
2016-06-18 11:35:31 +1200

    Successfully executes JavaScript test code

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const fs = require('fs');
+const exec = require('child_process').exec;
 
 const SCRIPT_FILE = 'src/albumize-disc-groups.js.applescript';
 
@@ -9,6 +10,12 @@ function log(tag, data) {
 
 gulp.task('default', function () {
     var scriptContents = fs.readFileSync(SCRIPT_FILE, 'utf8');
-    log('Script Contents', scriptContents);
+    // log('Script Contents', scriptContents);
 
+    var TEST_BASH_COMMAND = 'osascript -l JavaScript -e "\'hi\'"';
+    exec(TEST_BASH_COMMAND, (error, stdout, stderr) => {
+        log('Error', error);
+        log('stdout', stdout);
+        log('stderr', stderr);
+    });
 });
