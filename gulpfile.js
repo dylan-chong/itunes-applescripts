b46174ae875fde893c7b66c6749e751bd5f76676
Dylan Chong <dylan.chong@icloud.com>
2016-06-20 14:18:07 +1200

    Remove unused constants

--- gulpfile.js
+++ gulpfile.js
@@ -14,9 +14,7 @@ const OPTION_DEFINITIONS = [
 ];
 const OPTIONS = commandLineArgs(OPTION_DEFINITIONS);
 
-const SCRIPT_FILE = 'src/albumize-disc-groups.js.applescript';
 const SRC = 'src/*.js.applescript';
-const DEST = 'dist/';
 
 
 
