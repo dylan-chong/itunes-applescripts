8244934909da77f0365f5b5e3f0ed41167bb45b6
Dylan Chong <dylan.chong@icloud.com>
2016-06-24 20:05:02 +1200

    Moved logging into node_modules-local/header-log.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const commandLineArgs = require('command-line-args');
 const gulp = require('gulp');
 const watch = require('gulp-watch');
 
-const fancyLog = require('fancy-log');
+const log = require('./node_modules-local/header-log.js').log;
 
 // **************** CONSTANTS **************** //
 
@@ -18,44 +18,6 @@ const OPTION_DEFINITIONS = [
     { name: EXECUTE_JS_OSA_FILE_COMMAND_LINE_NAME, alias: 'e', type: String }
 ];
 const OPTIONS = commandLineArgs(OPTION_DEFINITIONS);
-
-// **************** BASIC LOGGING  **************** //
-
-function log(tag, priority, data) {
-    switch (priority) {
-        case 0:
-            var LINE_COUNT = 3;
-            var MARK_COUNT = 6;
-            var marks = Array(MARK_COUNT + 1).join('*');
-            var obviousLine = marks +
-                Array(tag.length + 1 + 2).join('-') +
-                marks;
-            // + 1 because array.join creates one less copy of the string
-            // than the count, and + 2 because of the spaces around the tag
-            for (var a = 0; a < LINE_COUNT; a++) {
-                fancyLog(obviousLine);
-            }
-            fancyLog(marks + ' ' + tag + ' ' + marks);
-            for (var a = 0; a < LINE_COUNT; a++) {
-                fancyLog(obviousLine);
-            }
-            break;
-        case 1:
-            fancyLog();
-            fancyLog('****** ' + tag + ' ******\n');
-            break;
-        case 2:
-            fancyLog('** ' + tag + ' **\n');
-            break;
-        case 3:
-            fancyLog('** ' + tag + ' **');
-            break;
-        default:
-            console.error('Invalid priority: ' + priority);
-            break;
-    }
-    if (data) console.log('\t' + data.toString().replace(/\n/g, '\n\t'));
-}
 
 // **************** TASKS **************** //
 
