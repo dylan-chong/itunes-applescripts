a272642f5f87e30327d4e35c9f307694f2cc916b
Dylan Chong <dylan.chong@icloud.com>
2016-06-24 20:53:42 +1200

    Converted 4 spaces to 2 spaces in gulpfile and local node modules

--- gulpfile.js
+++ gulpfile.js
@@ -14,20 +14,22 @@ const SRC = 'src/*.js.applescript';
 
 // Command line args
 const EXECUTE_JS_OSA_FILE_COMMAND_LINE_NAME = 'execute-js-osa-file';
-const OPTION_DEFINITIONS = [
-    { name: EXECUTE_JS_OSA_FILE_COMMAND_LINE_NAME, alias: 'e', type: String }
-];
+const OPTION_DEFINITIONS = [{
+  name: EXECUTE_JS_OSA_FILE_COMMAND_LINE_NAME,
+  alias: 'e',
+  type: String
+}];
 const OPTIONS = commandLineArgs(OPTION_DEFINITIONS);
 
 // **************** TASKS **************** //
 
 gulp.task('default', function () {
-    var commandLineArgFile = OPTIONS[EXECUTE_JS_OSA_FILE_COMMAND_LINE_NAME];
-    if (commandLineArgFile) {
-        osa.executeJsFile(commandLineArgFile);
-        return;
-    }
+  var commandLineArgFile = OPTIONS[EXECUTE_JS_OSA_FILE_COMMAND_LINE_NAME];
+  if (commandLineArgFile) {
+    osa.executeJsFile(commandLineArgFile);
+    return;
+  }
 
-    log('Watching for changes', 2);
-    watch(SRC).on('change', osa.executeJsFile);
+  log('Watching for changes', 2);
+  watch(SRC).on('change', osa.executeJsFile);
 });
