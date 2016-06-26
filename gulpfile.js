4ee1be2c0f4ea6b4af68093580da651eddcd0207
Dylan Chong <dylan.chong@icloud.com>
2016-06-26 17:11:33 +1200

    Implemented saveTemplateString

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 // **************** DEPENDENCIES **************** //
 
 const commandLineArgs = require('command-line-args');
+const fs = require('fs');
 
 const gulp = require('gulp');
 const watch = require('gulp-watch');
@@ -20,8 +21,9 @@ const FILES = {
   SCRIPTS: DIRECTORIES.SCRIPTS + '*/script.js.applescript',
   SCRIPT_DESCRIPTIONS: DIRECTORIES.SCRIPTS + '*/description.txt',
   BUILD_TEMPLATE: 'src/build/build-template.js.applescript',
-  TINY_CORE_LIBRARY: 'bower_components/TinyCore.js/build/TinyCore.min.js',
+  TINY_CORE_LIBRARY: 'bower_components/TinyCore.js/build/TinyCore.min.js'
 };
+const BUILT_SCRIPT_EXTENSION = '.js.applescript';
 
 // Command line args
 const EXECUTE_JS_OSA_FILE_COMMAND_LINE_NAME = 'execute-js-osa-file';
@@ -88,9 +90,10 @@ function buildScript(scriptFileToCompile) {
   }
 
   function saveTemplateString(scriptFileToCompile, templateString) {
-    var scriptName = getScriptName(scriptFileToCompile);
-    // TODO  return path that it was saved at
-    
+    var scriptPath = DIRECTORIES.BUILD + getScriptName(scriptFileToCompile);
+    fs.writeFileSync(scriptPath, templateString);
+    return scriptPath;
+
     function getScriptName(scriptFileToCompile) {
       var pathParts = scriptFileToCompile.split('/');
       return pathParts[pathParts.length - 1];
