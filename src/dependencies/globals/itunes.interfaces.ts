47e6bcfe1a01867e23a032c0a030fe7560a434fb
Dylan Chong <dylan.chong@icloud.com>
2017-01-05 17:26:40 +1300

    [update-queue] Added calculating days until track is ready to play

--- src/dependencies/globals/itunes.interfaces.ts
+++ src/dependencies/globals/itunes.interfaces.ts
@@ -4,12 +4,22 @@ interface IApplication {
 }
 
 type ApplicationGlobal = (appName: string) => IApplication;
+declare var Application: ApplicationGlobal;
 
 interface IPlaylist {
   name: () => string;
-  tracks: () => ITrack;
+  tracks: () => ITrack[];
 }
 
 interface ITrack {
-  name: { (): string, set: (value: string) => void }
+  name: IGettableSettable<string>;
+  /** Between 0 and 100. Divide by 20 to get number of stars */
+  rating: IGettableSettable<number>;
+  playedCount: IGettableSettable<number>;
+  playedDate: IGettableSettable<Date>;
+}
+
+interface IGettableSettable<T> {
+  (): T;
+  set: (value: T) => void
 }
--- src/scripts/update-queue/update-queue.script.ts
+++ src/scripts/update-queue/update-queue.script.ts
@@ -12,29 +12,105 @@ function createScript(): Script {
 
     // *************************
 
-    var appGlobal: ApplicationGlobal = Application;
-    var app: IApplication = appGlobal('iTunes');
+    var app = Application('iTunes');
     app.includeStandardAdditions = true;
 
     var allMusicPlaylist = getPlaylist(ALL_MUSIC_PLAYLIST);
     var queuePlaylist = getPlaylist(QUEUE_PLAYLIST);
 
-    var allTracks = allMusicPlaylist.tracks();
+    var allTracks = allMusicPlaylist.tracks()
+      .slice(426, 456);
+    var readyTracks: ITrack[] = [];
+    var unreadyTracks: ITrack[] = [];
+    divideTracks(allTracks, readyTracks, unreadyTracks);
 
     debugger;
-
 
     return 'Done';
 
     function getPlaylist(name: string): IPlaylist {
-      var playlists: IPlaylist[] = app.playlists();
-      var matches = playlists
+      var matches = app.playlists()
         .filter((playlist) => playlist.name() == name);
 
       if (matches.length < 1) throw 'No match for playlist: ' + name;
       if (matches.length > 1) throw 'Multiple matches for playlist: ' + name;
 
       return matches[0];
+    }
+
+    function divideTracks(tracks: ITrack[],
+                          readyTracks: ITrack[],
+                          unreadyTracks: ITrack[]): void {
+      debugger;
+
+      tracks.forEach((track) => {
+        if (getDaysUntilTrackIsReady(track) < 0) {
+          readyTracks.push(track);
+        }
+        else unreadyTracks.push(track);
+      });
+
+      /**
+       * @param track
+       * @return {number} Less than 0 if already ready to play
+       */
+      function getDaysUntilTrackIsReady(track: ITrack): number {
+        var readyDay = getLaterDate(track.playedDate(), getWaitDays(track));
+        var now = new Date();
+        return daysBetween(now, readyDay);
+
+        function daysBetween(from: Date, to: Date): number {
+          var millisDifference = to.getTime() - from.getTime();
+          return millisDifference / (24 * 60 * 60 * 1000);
+        }
+
+        function getLaterDate(date: Date, numDays: number): Date {
+          var laterDate = new Date(date.getTime());
+          laterDate.setDate(laterDate.getDate() + numDays);
+          return laterDate;
+        }
+
+        /**
+         * If track is played within this number of days, the track is not
+         * allowed to enter the QUEUE_PLAYLIST (i.e., is is ready)
+         * @param track
+         */
+        function getWaitDays(track: ITrack): number {
+          var NOT_PLAYED = 0;
+
+          var playedCountGroup: number = getPlayedCountGroup(track);
+          if (playedCountGroup === NOT_PLAYED) {
+            return 0;
+          }
+
+          var stars = track.rating() / 20;
+          var baseDays: number = getBaseDays(stars);
+          if (playedCountGroup >= 2) baseDays += 30;
+          if (playedCountGroup >= 3) baseDays += 20;
+          if (playedCountGroup >= 4) baseDays += 10;
+
+          return baseDays;
+
+          function getPlayedCountGroup(track: ITrack): number {
+            var plays = track.playedCount();
+            if (plays === 0) return NOT_PLAYED;
+            if (plays <= 3) return 1;
+            if (plays <= 8) return 2;
+            if (plays <= 18) return 3;
+            return 4;
+          }
+
+          function getBaseDays(stars: number): number {
+            switch (stars) {
+              case 1: return 310;
+              case 2: return 250;
+              case 3: return 190;
+              case 4: return 160;
+              case 5: return 130;
+            }
+          }
+        }
+      }
     }
   }
 }
