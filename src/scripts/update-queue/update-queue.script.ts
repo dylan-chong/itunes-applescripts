3b413cef217d590c5db21d14c1f7e539bbb4ea79
Dylan Chong <dylan.chong@icloud.com>
2017-01-06 00:09:09 +1300

    [update-queue] Added limit so efficient method doesn't make it worse

--- src/scripts/update-queue/update-queue.script.ts
+++ src/scripts/update-queue/update-queue.script.ts
@@ -11,6 +11,8 @@ function createScript(): Script {
     var ALL_MUSIC_PLAYLIST = 'All Playable Music';
     var QUEUE_PLAYLIST = 'Queue';
 
+    var TRACK_LIMIT = 300;
+
     // *************************
 
     var app = Application('iTunes');
@@ -21,18 +23,29 @@ function createScript(): Script {
     var queuePlaylist = getPlaylist(QUEUE_PLAYLIST);
 
     // Process tracks
-    var allTracks = allMusicPlaylist.tracks()
-      .slice(0, 100);
+    var allTracks = allMusicPlaylist.tracks();
     var wrappedReadyTracks = wrapReadyTracks(allTracks);
-    debugger; // todo
+
     wrappedReadyTracks.sort((w1, w2) => {
       return w1.getDaysUntilReady() - w2.getDaysUntilReady();
     });
 
-    clearPlaylist(queuePlaylist, wrappedReadyTracks);
-    addAllToPlaylist(queuePlaylist, wrappedReadyTracks);
+    wrappedReadyTracks.forEach(wrapper => {
+      var d = -Math.round(wrapper.getDaysUntilReady());
+      console.log('Has been ready for ' + d + ' days:\t' +
+        wrapper.getTrack().name());
+    });
+
+    clearPlaylist(queuePlaylist, wrappedReadyTracks, shouldKeepExistingTracks());
+    addAllToPlaylist(queuePlaylist, wrappedReadyTracks, shouldKeepExistingTracks());
 
     return 'Done';
+
+    function shouldKeepExistingTracks(): boolean {
+      if (wrappedReadyTracks.length > 100) return false;
+      if (queuePlaylist.tracks().length > 100) return false;
+      return true;
+    }
 
     function getPlaylist(name: string): IPlaylist {
       var matches = app.playlists()
@@ -45,20 +58,26 @@ function createScript(): Script {
     }
 
     function clearPlaylist(playlist:IPlaylist,
-                           tracksWrappersToKeep: TrackReadinessWrapper[]) {
+                           tracksWrappersToKeep: TrackReadinessWrapper[],
+                           shouldKeepExistingTracks: boolean) {
       var tracksToKeep = tracksWrappersToKeep.map(wrapper => wrapper.getTrack());
       playlist.tracks().forEach((track) => {
-        if (arrayOfTracksContainsTrack(tracksToKeep, track)) return;
+        if (shouldKeepExistingTracks &&
+          arrayOfTracksContainsTrack(tracksToKeep, track)) {
+          return;
+        }
         track.delete({from: playlist});
       });
     }
 
     function addAllToPlaylist(playlist:IPlaylist,
-                              tracksWrappersToAdd: TrackReadinessWrapper[]) {
+                              tracksWrappersToAdd: TrackReadinessWrapper[],
+                              shouldKeepExistingTracks: boolean) {
       tracksWrappersToAdd.forEach((wrapper) => {
-        var existingTrack = firstMatchingTrackInArray(
-          playlist.tracks(),
-          wrapper.getTrack());
+        if (shouldKeepExistingTracks)
+          var existingTrack = firstMatchingTrackInArray(
+            playlist.tracks(),
+            wrapper.getTrack());
 
         if (existingTrack) {
           existingTrack.move({to: playlist});
@@ -107,7 +126,8 @@ function createScript(): Script {
      */
     function wrapReadyTracks(tracks: ITrack[]): TrackReadinessWrapper[] {
       var wrapped: TrackReadinessWrapper[] = [];
-      tracks.forEach((track) => {
+      tracks.forEach((track, index) => {
+        if (wrapped.length >= TRACK_LIMIT) return;
         var days = getDaysUntilTrackIsReady(track);
         if (days > 0) return;
         wrapped.push(new TrackReadinessWrapper(track, days));
