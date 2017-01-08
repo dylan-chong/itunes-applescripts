16a551ee69ff50e03c27974aff7ddd0673d79685
Dylan Chong <dylan.chong@icloud.com>
2017-01-08 16:02:11 +1300

    [gulp] Show help table on `gulp` or `gulp help`

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,8 @@ const typescript = require('typescript-compiler');
 const mkdirp = require('mkdirp');
 
 const log = require('./node_modules-local/header-log.js').log;
+const Table = require('cli-table');
+
 const osa = require('./node_modules-local/execute-osa.js');
 const getFilledString = require('./node_modules-local/fill.js').getFilledString;
 
@@ -83,11 +85,13 @@ var taskObjects = [];
 function createTask(taskObj) {
   gulp.task(taskObj.taskName, taskObj.taskDependencies, taskObj.taskFunction);
 
-  if (taskObj.taskAliasNames) {
-    taskObj.taskAliasNames.forEach(function (alias) {
-      gulp.task(alias, [taskObj.taskName]);
-    });
-  }
+  // Avoid having to deal with nulls
+  if (!taskObj.taskAliasNames) taskObj.taskAliasNames = [];
+  if (!taskObj.taskDependencies) taskObj.taskDependencies = [];
+
+  taskObj.taskAliasNames.forEach(function (alias) {
+    gulp.task(alias, [taskObj.taskName]);
+  });
 
   taskObjects.push(taskObj);
 }
@@ -95,10 +99,11 @@ function createTask(taskObj) {
 // **************** DEFAULT **************** //
 
 createTask({
-    taskName: 'default',
-    taskDependencies: ['help'],
-    taskFunction: defaultTask,
-    taskAliasNames: []
+  taskName: 'default',
+  taskDependencies: ['help'],
+  taskFunction: defaultTask,
+  taskAliasNames: [],
+  taskDescription: 'Default task when running `gulp` without choosing a task'
 });
 
 function defaultTask() {
@@ -111,10 +116,42 @@ createTask({
   taskName: 'help',
   taskDependencies: [],
   taskFunction: helpTask,
-  taskAliasNames: ['h']
+  taskAliasNames: ['h'],
+  taskDescription: 'Displays this help page'
 });
 
 function helpTask() {
+  var sortedTaskObjects = taskObjects.slice(0, taskObjects.length);
+  sortedTaskObjects.sort(function (taskObjA, taskObjB) {
+    // Make default the first task in the list
+    if (taskObjA.taskName === 'default') return -1;
+    if (taskObjB.taskName === 'default') return 1;
+
+    if (taskObjA.taskName < taskObjB.taskName) return -1;
+    if (taskObjA.taskName > taskObjB.taskName) return 1;
+    throw 'What?';
+  });
+
+  var table = new Table({head: [
+    // Table column headers
+    'Task', 'Aliases', 'Task Dependencies', 'Description'
+  ]});
+
+  sortedTaskObjects.forEach(function (taskObj) {
+    table.push([
+      getTaskName(taskObj),
+      taskObj.taskAliasNames.join(', '),
+      taskObj.taskDependencies.join(', '),
+      taskObj.taskDescription || ''
+    ]);
+
+    function getTaskName(taskObj) {
+      if (taskObj.taskName === 'default') return '<default>';
+      return taskObj.taskName;
+    }
+  });
+
+  log('These are the tasks you can run', 1, table.toString() + '\n');
 }
 
 // **************** BUILDING **************** //
@@ -123,7 +160,9 @@ createTask({
   taskName: 'build',
   taskDependencies: [],
   taskFunction: buildTask,
-  taskAliasNames: ['b']
+  taskAliasNames: ['b'],
+  taskDescription: 'Builds all scripts, or a single one using the ' +
+  '`--script script-name` argument'
 });
 
 function buildTask() {
@@ -252,11 +291,13 @@ createTask({
   taskName: 'execute',
   taskDependencies: [],
   taskFunction: executeTask,
-  taskAliasNames: ['e']
+  taskAliasNames: ['e'],
+  taskDescription: 'Executes a single script which is chosen using the ' +
+  '`--script script-name` argument'
 });
 
 function executeTask(done) {
-  requireSelectedScriptArg('execute', executeScript);
+  requireSelectedScriptArgTask('execute', executeScript);
 
   function executeScript(userEnteredScriptName) {
     osa.executeJsFile(getScriptPath(userEnteredScriptName), done);
@@ -278,7 +319,8 @@ createTask({
   taskName: 'watch',
   taskDependencies: [],
   taskFunction: watchTask,
-  taskAliasNames: ['w']
+  taskAliasNames: ['w'],
+  taskDescription: 'Automatically rebuilds and executes the changed script (DANGEROUS!)'
 });
 
 function watchTask(done) {
@@ -315,29 +357,46 @@ function watchTask(done) {
 
 createTask({
   taskName: 'build-execute',
-  taskDependencies: [],
+  taskDependencies: ['require-selected-script-arg', 'build', 'execute'],
   taskFunction: buildAndExecuteTask,
-  taskAliasNames: ['be']
+  taskAliasNames: ['be'],
+  taskDescription: 'Builds and executes using the `--script script-name` argument'
 });
 
 function buildAndExecuteTask(done) {
-  requireSelectedScriptArg('build-execute', noop);
-  buildTask(noop);
-  executeTask(done);
-
-  function noop() {}
+  // Dependencies do the work
 }
 
 createTask({
   taskName: 'list-scripts',
   taskDependencies: [],
   taskFunction: listScriptsTask,
-  taskAliasNames: ['list', 'ls', 'l']
+  taskAliasNames: ['list', 'ls', 'l'],
+  taskDescription: 'Lists all the scripts in the src/scripts/ directory'
 });
 
 function listScriptsTask() {
   var scriptNames = getAllScriptNames();
   log('Scripts:', 1, scriptNames.join('\n') + '\n');
+}
+
+createTask({
+  taskName: 'require-selected-script-arg',
+  taskDependencies: [],
+  taskFunction: requireSelectedScriptArgTask,
+  taskAliasNames: [],
+  taskDescription: 'Just a helper task. Don\'t use me'
+});
+
+function requireSelectedScriptArgTask(doFuncThatTakesAScriptNameOrPath) {
+  if (tryDoWithSelectedScript(doFuncThatTakesAScriptNameOrPath)) {
+    return;
+  }
+
+  var example = 'gulp whatever-the-task-was --' + SCRIPT_COMMAND_LINE_ARG +
+    ' some-script-name-or-path';
+  throw 'ERROR: No script name detected. Try again with something like:' +
+  example;
 }
 
 // **************** OTHER **************** //
@@ -362,18 +421,6 @@ function tryDoWithSelectedScript(doFuncThatTakesAScriptNameOrPath) {
     }
   }
   return false;
-}
-
-function requireSelectedScriptArg(currentTaskName,
-                                  doFuncThatTakesAScriptNameOrPath) {
-  if (tryDoWithSelectedScript(doFuncThatTakesAScriptNameOrPath)) {
-    return;
-  }
-
-  var example = 'gulp ' + currentTaskName + ' --' + SCRIPT_COMMAND_LINE_ARG +
-    ' some-script-name-or-path';
-  throw 'ERROR: No script name detected. Try again with something like:' +
-  example;
 }
 
 /**
