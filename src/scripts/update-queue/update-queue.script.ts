660d51540962d48673eaf5500c9c38090f290d5a
Dylan Chong <dylan.chong@icloud.com>
2019-03-10 15:54:58 +1300

    Change update q maximum limit

--- src/scripts/update-queue/update-queue.script.ts
+++ src/scripts/update-queue/update-queue.script.ts
@@ -15,7 +15,7 @@ function createScript(): Script {
     /**
      * The maximum length of the resulting playlist, in seconds.
      */
-    var PLAYLIST_DURATION_LIMIT_SECONDS = 4 * 12 * 3600;
+    var PLAYLIST_DURATION_LIMIT_SECONDS = 25 * 3600;
 
     /**
      * Set to 0.2 to make sure no album takes up (much) more than 20% of
